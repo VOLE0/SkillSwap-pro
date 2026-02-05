f94bcdd618ff53f9e05ea02e233739da4bc9e3a1
VOLE0 <l.wagner2909@gmail.com>
2026-02-05 19:09:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import SkillSwapPro from './SkillSwapPro';
+import Success from './Success';
 
 function App() {
+  const path = window.location.pathname;
+  
+  if (path === '/success') {
+    return <Success />;
+  }
+  
   return (
     <div className="App">
       <SkillSwapPro />
